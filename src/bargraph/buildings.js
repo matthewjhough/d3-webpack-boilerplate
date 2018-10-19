5c263316ecd4bfddb887da19c20eee81b0f4b0a5
matthewjhough <matthough1@icloud.com>
2018-10-19 16:59:40 -0400

    updated axis/ labels

--- src/bargraph/buildings.js
+++ src/bargraph/buildings.js
@@ -1,22 +1,27 @@
 import * as d3 from "d3";
 
-var margin = { left: 100, right: 10, top: 10, bottom: 100 };
-var width = 600 - margin.left - margin.right;
-var height = 400 - margin.top - margin.bottom;
+var margin = { left: 100, right: 10, top: 10, bottom: 150 };
 
-var svg = d3
+var width = 600 - margin.left - margin.right,
+  height = 400 - margin.top - margin.bottom;
+
+// create canvas for visualization
+var g = d3
   .select("#chart-area")
   .append("svg")
   .attr("width", width + margin.left + margin.right)
-  .attr("height", height + margin.top + margin.bottom);
-
-var g = svg
+  .attr("height", height + margin.top + margin.bottom)
   .append("g")
   .attr("transform", "translate(" + margin.left + ", " + margin.top + ")");
 
+// Labels ...
+
+// Viz start
 d3.json("../data/buildings.json").then(data => {
+  // convert all height strings to int
   data.forEach(item => (item.height = +item.height));
 
+  // set x values
   var x = d3
     .scaleBand()
     .domain(data.map(d => d.name))
@@ -24,18 +29,36 @@ d3.json("../data/buildings.json").then(data => {
     .paddingInner(0.3)
     .paddingOuter(0.3);
 
+  // set y values
   var y = d3
     .scaleLinear()
     .domain([0, d3.max(data, d => d.height)])
     .range([0, height]);
 
+  var xAxisCall = d3.axisBottom(x);
+  g.append("g")
+    .attr("class", "x axis")
+    .attr("transform", "translate(0, " + height + ")")
+    .call(xAxisCall)
+    .selectAll("text") // select all labels, and transform to make them readable
+    .attr("y", "10")
+    .attr("x", "-5")
+    .attr("text-anchor", "end")
+    .attr("transform", "rotate(-40)");
+
+  var yAxisCall = d3.axisLeft(y);
+  g.append("g")
+    .attr("class", "y-axis")
+    .call(yAxisCall);
+
+  // build and color rectangles
   var rects = g
     .selectAll("rect")
     .data(data)
     .enter()
     .append("rect")
     .attr("x", d => x(d.name))
-    .attr("y", 50)
+    .attr("y", 0)
     .attr("width", x.bandwidth())
     .attr("height", d => y(d.height))
     .attr("fill", "gray");
